8c49b7f502ffa1f1a3b93e8f473ff13d43695d3b
Bruno Santos <brunojsfla@gmail.com>
2019-07-01 17:07:34 -0300

    Cors configurado

--- backend/config/server.js
+++ backend/config/server.js
@@ -3,9 +3,11 @@ const port = 3003;
 const bodyParser = require('body-parser');
 const express = require('express');
 const server = express();
+const allow = require('./cors');
 
 server.use(bodyParser.urlencoded({ extended: true}));
 server.use(bodyParser.json());
+server.use(allow);
 
 server.listen(port, function(){
     console.log(`BACKEND executando na porta ${port}.`);
